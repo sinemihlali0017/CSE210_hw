62ba7beb654834f7324dec5d70e62aca0aae13ec
sinemihlali0017 <sinemihlalimahlathini@gmail.com>
2025-05-14 00:24:29 +0200

    added names

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("what is your name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("what is your last_name?");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($" your name is {first} , {last_name}");
     }
 }
