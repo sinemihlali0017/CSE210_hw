9367b1413a4822c31a17757d95eb9b2bcef27df6
sinemihlali0017 <sinemihlalimahlathini@gmail.com>
2025-05-14 20:15:04 +0200

    calculations

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,61 @@
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        int userInput;
+
+        Console.WriteLine("Enter a series of numbers. Enter 0 to stop:");
+
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+
+        } while (userInput != 0);
+
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+        else
+        {
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+
+            double average = (double)sum / numbers.Count;
+
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Maximum: {max}");
+        }
     }
 }
