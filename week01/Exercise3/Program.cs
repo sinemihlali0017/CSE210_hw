62cd05b82eb9585f05047bee4eda03aca0979874
sinemihlali0017 <sinemihlalimahlathini@gmail.com>
2025-05-14 13:15:16 +0200

    added if statments

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+
+
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("what is your guess?");
+            string lucku_Guess = Console.ReadLine();
+            guess = int.Parse(lucku_Guess);
+
+            if (guess > magicNumber)
+            {
+                Console.Write("lower ");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.Write("higher");
+            }
+            else
+            {
+                Console.Write("you guessed it ");
+            }
+        }
     }
 }
+
+
+
+
+
