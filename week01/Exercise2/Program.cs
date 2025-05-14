9fec53dee14a88f6c7993db7d57c9a08cd4527e2
sinemihlali0017 <sinemihlalimahlathini@gmail.com>
2025-05-14 11:16:57 +0200

    if statments added

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,48 @@
+
 using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this time. Keep trying and you'll get there!");
+        }
     }
 }
